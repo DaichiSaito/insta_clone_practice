a35f66b90186c30af666e046fa9e0e4df4defa2c
DaichiSaito <hrkedz@gmail.com>
2019-12-30 21:29:37 +0900

    [Add]ページネーション実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
   def index
-    @posts = Post.all.includes(:user)
+    @posts = Post.all.includes(:user).page(params[:page]).order(created_at: :desc).per(10)
   end
 
   def new
